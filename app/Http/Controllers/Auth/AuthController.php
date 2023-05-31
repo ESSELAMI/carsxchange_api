67d8003a70fd87a106fa894a107cbf2d93976380
esselami.abdellatif <esselami.abdellatif@hotmail.fr>
2023-05-31 21:15:22 +0400

    ADDED PRINTING TICKET

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+
+    /*
+	 * Register new user
+	*/
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        if (User::create($validatedData)) {
+            return response()->json(null, 201);
+        } else
+
+            return response()->json(null, 404);
+    }
+
+    /*
+	 * Generate sanctum token on successful login
+	*/
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $user->createToken($request->email)->plainTextToken
+        ], 200);
+    }
+}
